5939766b7ddf881bfc01fddf1bda5dfd4870150c
Nat <nat@Nats-MBP.lan>
2020-09-01 22:04:38 -0700

    finished firebase/firestore auth

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from'./pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from'./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -24,12 +24,27 @@ class App extends React.Component {
   unsubscribeFromAuth = null
   //this is telling our application that the user has been authenticated, also provides persistence of user sessions. essentially a messaging system between our app and the firebase system. whenever any changes occur on firebase relating to this application, firebase sends us a message telling us the state has been updated, meaning they either signed in or out. when it detects this, firebase will send us the user below. need to prevent memory leaks by using method above.
   componentDidMount() {
-    //this line gives us a function that when called will close the subscription
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    //this function is essentially saying if someone logs in, and the auth state changes, it will execute. we check if userauth exists and then call our createuserprofiledoc to add the user to the database.
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        //when we call onsnapshot we get back the snapshot object which is where we get the data related to the user we just possibly stored if it was a new authentication or the data related to the user already stored in our database. we can use the .data method to actually retrieve that data. we store this data in an object in state called currentUser with the properties uid from the userauth object, along with the properties from the database, createdat, email, displayname, so that we can use the data from our database in our application. 
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        //if the userauth object comes back and its null, we want to set the currentuser to userAuth, which is null. so when the user logs out, we will set this currentuser to null
+      } else {
 
-      console.log(user);
-    })
+        this.setState({currentUser: userAuth});
+
+      }
+      
+    });
   }
   //we want to close the subscription whenever our component unmounts, so we call unsubscribefromauth when the component unmounts.
   componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,27 +1,66 @@
 //we do firebase/app because firebase has all of the utility libraries included. we dont want any were not using because they are large, so we can import individually the packages we need. we need to first import firebase to gain access to its elements, like auth and firebase.
 import firebase from 'firebase/app';
+
+//we use firestore to take the auth.user object, whether it be a login form a third party or from our own sign up method, and store it in our own database to authentice custom generated accounts
 import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyANJeElTFT5nfEW-qhQy2LCSY82W4Tm7tc",
-    authDomain: "the-spot-clothing.firebaseapp.com",
-    databaseURL: "https://the-spot-clothing.firebaseio.com",
-    projectId: "the-spot-clothing",
-    storageBucket: "the-spot-clothing.appspot.com",
-    messagingSenderId: "218277071443",
-    appId: "1:218277071443:web:f09c0a372fcb421149d4a6"
+  apiKey: "AIzaSyANJeElTFT5nfEW-qhQy2LCSY82W4Tm7tc",
+  authDomain: "the-spot-clothing.firebaseapp.com",
+  databaseURL: "https://the-spot-clothing.firebaseio.com",
+  projectId: "the-spot-clothing",
+  storageBucket: "the-spot-clothing.appspot.com",
+  messagingSenderId: "218277071443",
+  appId: "1:218277071443:web:f09c0a372fcb421149d4a6"
+};
+/*
+this function is gathering the data from the sign in with google, and storing it within firestore. it takes in the userAuth object that has been provided with the auth library. also takes in any additional data we might want. if the userAuth object doesnt exist, we exit the function. we only want it to run when someone signs in and we get a valid object. otherwise, we want to query inside of the firestore document to see if it already exists. when we query firestore either a document or collection, we get returned either a QueryReference object or a QuerySnapshot object. the reference is an object that represents the current place in the database we are calling for. docReference is used perform CRUD opererations. we can use set, get, update, delete. we can also add documents to collections using the collectionRef object .add method collectionRef.add({value:prop}). we get the snapshotObject from the refObject using the .get method. documentRef returns a documentSnapshot object and collectionRef returns a querySnapshot 
+*/
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get(); // we use the reference object to either get or save data at this location in db
+
+  // if a piece of data does not exist in that location, meaning the user has not been inputed into the db, so we check if it exists using our snapshot var. if it doesnt exist, we create it taking in the displayname and email from the userAuth object. we also add a createdAt using the js date function to store when the account was stored. 
+  if(!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    //here we use the .set method to store the three properties we defined above, along with any additional data we might store. we wrap it in a try catch block because it is an async request to our database to create this object, and we use the catch to log any errors. so if this snapshot doesnt exist, it will be created using the data from the userauth object.
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  //if we want to use this userRef object to do other things in different parts of our code, we must return it.
+  return userRef;
 };
 
-  // initializes app with config from above
-  firebase.initializeApp(config);
+// initializes app with config from above
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  // gives us access to googleauthprovider from the auth library
-  const provider = new firebase.auth.GoogleAuthProvider(); 
-  provider.setCustomParameters({prompt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+// gives us access to googleauthprovider from the auth library
+const provider = new firebase.auth.GoogleAuthProvider(); 
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export default firebase;
+export default firebase;
+
+
+
+
+
+  
